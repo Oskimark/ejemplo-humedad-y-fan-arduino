1b3faf7c3e1bb244affcc020de27a8b4550768de
Oskimark <elnona@gmail.com>
2020-02-17 00:30:08 -0300

    Update humedad_con_ventilador_y_alarma.ino
    codigo un poco mas pulido. me falta la parte de mandar el sensor a dormir  durante x minutos si esta todo bien.

--- humedad_con_ventilador_y_alarma.ino
+++ humedad_con_ventilador_y_alarma.ino
@@ -1,91 +1,40 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
+#include <DHT.h> //libreria para el lector de humedad
+#define DHTPIN 2 //el pin en el que leemos el valor
+#define DHTTYPE DHT11 
+#define tomarmedida 4 //en este pin va un transistor a modo de rele que activa un divisor de tension para medir la bateria
+float humedad;  //variable para la humedad
 
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+DHT dht(DHTPIN, DHTTYPE); //creamos un objeto del tipo DHT llamado dht muy original
 
-#include "DHT.h"
-
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
-
-#define DHTTYPE DHT11   // DHT 11
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
-
-
-DHT dht(DHTPIN, DHTTYPE); //creas un objeto del tipo DHT llamado dht, al que l epasas dos constantes que son el pin al que se conecta y el tipo de modulo usado.
 
 void setup() {
-  Serial.begin(9600);  //inicias la comunicacion serial por usb
-  Serial.println(F("DHTxx test!")); //mandas una linea de texto con salto de linea por eso es println
+dht.begin(); //iniciamos el sensor
 
-  dht.begin();  //este comando inicia el sensor
+
+}
+void loop() {    //este sera el loop de nuestro programa
+  leer_voltios()  //primero antes de medir la humedad y prender el ventilador medimos la bateria por las dudas, la funcion esta al final
+ delay(2000);
+ //me falta un if para decidir dependiendo de la bateria si continuamos o no.
+ //aca continua nomas
+dht.readHumidity(humedad);//lee la humedad y la pasa a la variable
+if (humedad>70) //si es mas que un valor determinado que es fijo pero se podria regular externamente  
+
+{digitalWrite(5, HIGH);//prendemos el ventilador
+delay(10000); //durante 10 segundos
+}
+//regresamos al inicio cuantas veces sea necesario mientras tengamos bateria
+ 
 }
 
-void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);//haces una espera de  2 segundos para que el sensor se estabilice y las medidas no peguen saltos
+void leer_voltios()//es una funcion para leer la carga de la bateria para cuando llega a un nivel no continuar gastando bateria y poder dar un aviso sonoro
+{
+ 
+digitalWrite(tomarmedida,HIGH);
+float  voltios;
+voltios= (analogRead(A0))*0,004887585533*4.9000;
+if (voltios<=2.50)
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();//lees el valor de humedad y lo guardas en una variable h
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature(); //lees la temperatura y la guardas en la variable t
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println(F("Failed to read from DHT sensor!"));//el sensor puede dar valores incorrectos por mala coneccion o por baja bateria  o otro problema, si esto ocurre lo reporta por serial
-    return;
-  }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
-
-//aca es donde tendrias que comparar el valor con la humedad que vos queres seteary actuar acorde. 
-if h > 60 (
-    sonar alarma durante 5 segundos
-    pinmode(3, high);
-    delay(5000);
-    pinmode(3, low);
-    //prender el ventilador por x segundos
-    pinmode(4, high);
-    delay(xsegundos);
-    pinmode(4, low);
-
-    //ahora podes optar por chequear otra vez l ahumedad y repetir el ciclo o ir directo a reposo profundo durante unos minutos ...
-    
-    
-    
-    
-    deepsleep(200000)
-    
-
-  
+{tone(6, 400, 10);//esto hace sonar un tono de 400hz durante 10 milisegundos
+}
 }
